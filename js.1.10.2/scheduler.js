6fc2ac0c376e730859dbf20540ea3e5f8da16b0f
Kyly Vass <kvass@ucsd.edu>
2014-03-27 03:20:49 -0700

    Huge changes, working half way. Not getting a acurate count of set in a group.

--- js.1.10.2/scheduler.js
+++ js.1.10.2/scheduler.js
@@ -17,19 +17,27 @@ $(document).ready(function() {
 
 // Find if there are any overlapping appts
 function makeGroup(col) {
+  var group_id = 0;
 
-  var status;
-
-  console.log("isOverlapping");
+  var status = false;
 
   col.find('.appt').each(function() {
-
-    console.log("1");
 
     var appt = $(this);
     appt.id = $(this).attr('id');
     appt.start = $(this).attr('data-start');
     appt.end = $(this).attr('data-end');
+
+
+    if (appt.attr('data-group') == null) {
+
+      appt.attr('data-group', group_id++);
+    }
+
+
+    // TODO Visually label the appts
+    appt.text("[id=" + appt.id + "] [data-group= " + appt.attr(
+      'data-group') + "]");
 
     // TODO Ask why this is needed
     appt.css({
@@ -39,12 +47,9 @@ function makeGroup(col) {
 
     col.find('.appt').each(function() { //for each appointment
 
-      console.log("2");
-
       //  if it's not the appointment working with above
       if ($(this).attr('id') != appt.id) {
 
-        console.log("3");
         // you can retrieve appt params like this, 
         var start = parseInt($(this).attr('data-start'));
 
@@ -53,24 +58,155 @@ function makeGroup(col) {
         var appt_start = parseInt(appt.start);
         var appt_end = parseInt(appt.end);
 
-
         // Find any overlap
         if (appt_start > end && appt_end < start || appt_end > start &&
           appt_start < end) {
 
-          console.log('4');
           status = true;
-        } else
-          status = false;
+
+          // this doesn't have a group? create one
+          if ($(this).attr('data-group') == null) {
+
+            $(this).attr('data-group', appt.attr('data-group'));
+
+          };
+
+        }
+
+        console.log("resize" + appt.id + " = " + status);
+
+        console.log(appt.id + " calling resizeGroup= " + ($(this).attr(
+          'data-set') == null && status));
+        // TODO 
+        if ($(this).attr('data-set') == null && status) {
+          resizeGroup(col, appt);
+        }
+
       }
     });
   });
 
-return status;
+
+  return status;
 }
 
-function makeSet() {
+function resizeGroup(col, appt) {
 
+
+  // Incrementing value to offset appts
+  var left_offset = 0;
+  var num_sets;
+  var box_size;
+  var group_selector = '.appt[data-group=' + appt.attr(
+    'data-group') + ']';
+
+  //Find sub groups. These will be item that do not overlap in
+  // the group
+  num_sets = makeSet(col, group_selector);
+
+  console.log("num_sets=" + num_sets);
+  // Size of boxes
+  var box_size = 100 / num_sets;
+
+  console.log("box_size=" + box_size);
+
+  col.find(group_selector).each(function() {
+    console.log($(this).attr('id') + " css left is " + (box_size * parseInt($(
+      this).attr(
+      'data-set'))) + '%');
+
+    $(this).css({
+      left: (box_size * parseInt($(this).attr('data-set'))) + '%',
+      width: box_size + '%'
+    });
+
+  });
+
+}
+
+function makeSet(col, group_selector) {
+
+  var set_id = 0;
+  var set_count;
+  var group_length;
+  var set_number;
+
+  // Find set in the group that don't overlap
+  col.find(group_selector).each(function() {
+
+    var appt = $(this);
+    appt.id = $(this).attr('id');
+    appt.start = $(this).attr('data-start');
+    appt.end = $(this).attr('data-end');
+
+    // Number of elements in the group.
+    // As sets sets make one element so as set are created
+    // the group will be subtracted from
+    group_length = $(group_selector).length;
+
+    console.log((appt.attr('data-set') == null) ? appt.id +
+      " needs to be set to "  + set_id : appt.id + " is set to " + appt.attr('data-set'));
+
+    console.log("set_id=" + set_id);
+    
+    if (appt.attr('data-set') == null) {
+
+      appt.attr('data-set', set_id);
+
+      console.log(appt.id + " has been assigned to " + appt.attr('data-set'));
+    }
+    
+    set_id++;
+
+    appt.text("[id=" + appt.id + "] [data-group= " + appt.attr(
+      'data-group') + "] [data-set=" + appt.attr('data-set') + "]");
+
+    col.find(group_selector).each(function() {
+
+      //  if it's not the appointment working with above
+      if ($(this).attr('id') != appt.id) {
+
+        // Convert start and end times to integer values
+        // for comparison
+        var start = parseInt($(this).attr('data-start'));
+        var end = parseInt($(this).attr('data-end'));
+        var appt_start = parseInt(appt.start);
+        var appt_end = parseInt(appt.end);
+
+        console.log(appt.id + " comparing with " + $(this).attr('id'));
+
+        // Find members that do not overlap
+        if ((appt_start <= end || appt_end >= start) && (appt_end <= start ||
+          appt_start >= end)) {
+
+          console.log(appt.id + " does not overlap with " + $(this).attr(
+            'id'));
+
+          // this doesn't have a group? create one
+          if ($(this).attr('data-set') == null) {
+
+            $(this).attr('data-set', appt.attr('data-set'));
+
+            console.log("====================================================");
+            console.log("<" + $(this).attr('id') +
+              " has been assigned to set " + appt.attr('data-set') + ">");
+            console.log("====================================================");
+
+          }
+
+        } else{
+
+          console.log(appt.id + " does overlap with " + $(this).attr(
+            'id'));
+        }
+
+      }
+    });
+    set_number = parseInt(appt.attr('data-set'));
+    console.log("===>" + typeof(set_number));
+  });
+  console.log(set_number +" + "+ 1 + " = " + (set_number + 1));
+  return set_number + 1;
 }
 
 function stackCols() {
@@ -78,159 +214,10 @@ function stackCols() {
 
 
   // This is the function that needs work
-  $('.appt_canvas').each(function() { // run through each collumn
+  $('.appt_canvas').each(function() { // run through each column
     var col = $(this);
-    var group_id = 0;
-
-    console.log(isOverlapping(col));
-
-    // Counter to better understand iteration of the find function
-    var iter0 = 0;
-
-    // find each one of this collumns appointments
-    $(this).find('.appt').each(function() {
-
-      var iter1 = 0; // This is used in debug messages
-      var appt = $(this);
-      appt.id = $(this).attr('id');
-      appt.start = $(this).attr('data-start');
-      appt.end = $(this).attr('data-end');
-
-
-      if (appt.attr('data-group') == null) {
-
-        appt.attr('data-group', group_id++);
-      }
-
-
-      // TODO Visually label the appts
-      appt.text("id: " + appt.id + ",  data-group: " + appt.attr(
-        'data-group'));
-
-      // @Debug
-      if (debug_finest) {
-        iter0++;
-        console.log("appt data-group: " + appt.attr('data-group'));
-        console.log(iter0 + ":Outer find is running on " + appt.id);
-      };
-
-
-      // if your browser doesn't have a console, install firebug
-      appt.css({
-        top: apptTop(appt.start) + 'px',
-        height: apptHeight(appt.start, appt.end) + 'px'
-      });
-
-      // All good up to this point. The height of the appointment and the 
-      // appointments distance to the top are easy to figure out. What we 
-      // need to do is compare the appointment to every other appointment
-      // in the same col
-      col.find('.appt').each(function() { //for each appointment
-
-
-        // @Debug
-        if (debug_finest) {
-          iter1++;
-          console.log(iter0 + "." + iter1 + ":Inner find is running on " +
-            $(this).attr('id'));
-        }
-
-
-        //  if it's not the appointment working with above
-        if ($(this).attr('id') != appt.id) {
-
-          // you can retrieve appt params like this, 
-          var start = parseInt($(this).attr('data-start'));
-
-          // or set them like:  $(this).attr('data-whatever', 'value');
-          var end = parseInt($(this).attr('data-end'));
-          var appt_start = parseInt(appt.start);
-          var appt_end = parseInt(appt.end);
-
-
-          if (debug_finer) {
-            console.log($(this).attr('id') + " is being compared to " +
-              appt.id);
-          };
-
-          // Find any overlap
-          if (appt_start > end && appt_end < start || appt_end > start &&
-            appt_start < end) {
-
-            // @Debug f+
-            if (debug_fine) {
-              console.log(appt.attr('id') + " overlaps " + $(this).attr(
-                'id') + "\n");
-            }
-
-
-            // this doesn't have a group? create one
-            if ($(this).attr('data-group') == null) {
-
-              $(this).attr('data-group', appt.attr('data-group'));
-
-              // @Debug f+
-              if (debug_fine) {
-                console.log($(this).attr('id') + " is in the group " +
-                  $(this).attr("data-group"));
-              };
-
-            }
-
-          }
-
-          // If no overlap was found found this is not in a group
-          // next overlap will be in a new group
-          else if ($(this).attr('data-group') == null) {
-
-            // Incrementing value to offset appts
-            var left_offset = 0;
-            var group_length;
-            var box_size;
-            var group_selector = '.appt[data-group=' + appt.attr(
-              'data-group') + ']';
-
-            //Find sub groups. These will be item that do not overlap in
-            // the group
-            col.find(group_selector).each(function() {
-              // Select on Item than check through all the item
-              // and see if there is an item that does not overlap
-              // then add another tag 'sub-group' and number the 
-              // subgroups. 
-            });
-
-            group_length = $(group_selector).length;
-
-            // Size of boxes
-            var box_size = 100 / group_length;
-
-            col.find(group_selector).each(function() {
-
-              $(this).css({
-                left: (box_size * left_offset++) + '%',
-                width: box_size + '%'
-              });
-
-            });
-
-            // @Debug
-            if (debug_finer) {
-              console.log($(this).attr('id') + " does not overlap " +
-                appt.id);
-              console.log("its group remains " + $(this).attr(
-                'data-group'));
-            }
-          }
-
-        }
-
-        // @Debug
-        if (debug_finest) {
-          console.log("group_id: " + group_id);
-        }
-
-      });
-    });
+    var appt = null;
+    makeGroup(col);
   });
 }
 
